38a9d1cb4740a6c7ee48362bf3112bfcd4bb6846
Riley Johnston <rileydanejohnston@gmail.com>
2021-12-15 00:46:14 -0600

    feat: add ErrorManager to auth middleware

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,26 +1,22 @@
 const jwt = require('jsonwebtoken');
+const ErrorManager = require('../errors/error-manager');
 
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith('Bearer')) {
-    return res
-      .status(403)
-      .send({ message: 'Authorization required' });
+    next(new ErrorManager(403, 'asdfasdf Authorization required'));
   }
 
   // extract token
   const token = authorization.replace('Bearer ', '');
 
-  // try extracting payload - user id?
   let payload;
   try {
     payload = jwt.verify(token, 'secret-key');
   }
   catch (err) {
-    return res
-      .status(403)
-      .send({ message: 'Authorization required' });
+    next(new ErrorManager(403, 'Authorization required'));
   }
 
   req.user = payload;
