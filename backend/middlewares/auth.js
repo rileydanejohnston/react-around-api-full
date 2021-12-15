bcacbb57cec27407155e48d949bcaa7da8a20124
Riley Johnston <rileydanejohnston@gmail.com>
2021-12-14 18:20:55 -0600

    feat: create auth.js middleware

---
+++ backend/middlewares/auth.js
@@ -0,0 +1,28 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = (req, res, next) => {
+  const { authorization } = req.headers;
+
+  if (!authorization || !authorization.startsWith('Bearer')) {
+    return res
+      .status(401)
+      .send({ message: 'Authorization required' });
+  }
+
+  // extract token
+  const token = authorization.replace('Bearer ', '');
+
+  // try extracting payload - user id?
+  let payload;
+  try {
+    payload = jwt.verify(token, 'secret-key');
+  }
+  catch (err) {
+    return res
+      .status(401)
+      .send({ message: 'Authorization required' });
+  }
+
+  req.user = payload;
+  next();
+}
