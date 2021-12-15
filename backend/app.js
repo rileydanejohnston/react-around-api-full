7f43991ec2638cff984af93a16f03bc954acceee
Riley Johnston <rileydanejohnston@gmail.com>
2021-12-14 20:30:41 -0600

    fix: move /signin and /singup routes to app.js

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const helmet = require('helmet');
+const { celebrate, Joi } = require('celebrate');
 const userRouter = require('./routes/users');
 const cardRouter = require('./routes/cards');
+const { createUser, login } = require('./controllers/users');
+
 
 const { PORT = 3000 } = process.env;
 
@@ -27,6 +30,17 @@ app.use((req, res, next) => {
 
 app.use('/', userRouter);
 app.use('/', cardRouter);
+
+app.post('/signin', login);
+app.post('/signup', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string().min(2).max(30),
+    email: Joi.string().required(),
+    password: Joi.string().min(8).required(),
+  })
+}),createUser);
 
 app.get('*', (req, res) => {
   res.status(404).send({ message: 'Requested resource not found' });
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,12 +1,9 @@
 const router = require('express').Router();
-const { celebrate, Joi } = require('celebrate');
 const auth = require('../middlewares/auth');
 const {
   getUsers,
   getUser,
   getCurrentUser,
-  createUser,
-  login,
   updateProfile,
   updateAvatar,
 } = require('../controllers/users');
@@ -16,15 +13,5 @@ router.get('/users/:userId', getUser);
 router.get('/users/me', getCurrentUser);
 router.patch('/users/me', updateProfile);
 router.patch('/users/me/avatar', updateAvatar);
-router.post('/signin', login);
-router.post('/signup', celebrate({
-  body: Joi.object().keys({
-    name: Joi.string().min(2).max(30),
-    about: Joi.string().min(2).max(30),
-    avatar: Joi.string().min(2).max(30),
-    email: Joi.string().required(),
-    password: Joi.string().min(8).required(),
-  })
-}),createUser);
 
 module.exports = router;
